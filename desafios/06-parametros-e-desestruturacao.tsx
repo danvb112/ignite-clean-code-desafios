b4a718b25ba0324c0c78153b4b979f63e77a62ec
danvb112 <danielbonasser@gmail.com>
2025-04-01 20:10:53 -0300

    apply destructuring to function parameters

--- desafios/06-parametros-e-desestruturacao.tsx
+++ desafios/06-parametros-e-desestruturacao.tsx
@@ -1,11 +1,30 @@
-function updateUserRoute(body, params) {
-  updateUserController(body, params)
+
+function updateUserRoute({
+  body, 
+  params
+}) {
+  updateUserController({
+    data: body, 
+    params
+  })
 }
 
-function updateUserController(data, params) {
-  userRepository.update(data, params)
+function updateUserController({data, params}) {
+  const {name, email, password} = data
+  const {id} = params
+  userRepository.update({
+    email,
+    id,
+    name,
+    password
+  })
 }
 
 const userRepository = {
-  update: (data, params) => {},
+  update: ({
+    name,
+    email,
+    password,
+    id
+  }) => {},
 }
